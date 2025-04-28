4bf0a647774252d99699c6ef35abe2fa47109a85
atikmahmud482 <atikmahmud482@gmail.com>
2025-04-28 22:30:38 +0600

    math.floor()

--- Scrim-s01ua8a/index2.js
+++ Scrim-s01ua8a/index2.js
@@ -28,7 +28,7 @@ let totalTime = getTotalRaceTime();
 console.log(totalTime);
  */
 
-let randomNumber = Math.random() * 6;
+/* let randomNumber = Math.random() * 6;
 
 console.log(randomNumber);
 
@@ -38,5 +38,18 @@ In which range will our randomNumber be now?
 
 From: 0
 To: 5.999999
+
+*/
+// let randomNumber = Math.random() * 6
+
+let flooredNumber = Math.floor(12);
+
+console.log(flooredNumber);
+
+/* 
+
+What does Math.floor() do to positive numbers?
+
+Your answer: it removes the decimals
 
 */
